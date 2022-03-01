33131207f3912e417457bf25daa6ab28ddfb3ee3
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-01 12:32:43 +0100

    working with class

--- src/component/KontaktOs.js
+++ src/component/KontaktOs.js
@@ -3,9 +3,27 @@ import React from "react";
 
 
 
+class KontaktOs extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+export default KontaktOs
+
+
+
+
+
+/*
 function KontaktOs(){
-
-
     return(
         <div>
             <h1>
@@ -15,6 +33,5 @@ function KontaktOs(){
 
     )
 }
-
-
 export default KontaktOs
+*/
