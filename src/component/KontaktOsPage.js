4cfb512c8fb58220bb89ed8fff0cb1bf93506dff
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-07 21:09:51 +0100

    using grid for all button

--- src/component/KontaktOsPage.js
+++ src/component/KontaktOsPage.js
@@ -20,24 +20,29 @@ function KontaktOsPage () {
               alt="a image of a frugt"
               />
 
+
+            <div class="grid-container">
             <button
              id="b1" 
              type="button" 
-             class="btn btn-outline-primary"
+             class="grid-item"
              onClick={() => window.location = 'tel:+451234567'}
              >Kontakt os  +4512345678</button>
 
             <button
              id="b2" 
              type="button"
-              class="btn btn-outline-primary"
+              class="grid-item"
               onClick={() => window.location = 'mailto:info@crepecafe'}
               >Skriv til os info@crepecafe.dk</button>
 
             <button 
             id="b3" 
             type="button"
-             class="btn btn-outline-primary">Find os København</button>
+             class="grid-item">Find os København</button>
+
+            </div>
+            
 
         </div>
     )
