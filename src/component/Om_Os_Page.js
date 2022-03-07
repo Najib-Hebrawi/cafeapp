c39c4aefc3e2c2627b985cd9aa35f926097e2e12
Zeinab09 <s205432@student.dtu.dk>
2022-03-07 11:52:43 +0100

    Om_os page done

--- src/component/Om_Os_Page.js
+++ src/component/Om_Os_Page.js
@@ -21,13 +21,13 @@ function Om_Os_Page () {
                 DESSERTHUS I HJERTET AF BRØNSHØJ!
              </h1>
             <p>
-            Crepe Cafe laver selv den luftige pandekagedej, der giver små, flotte 
+            Crepe Café laver selv den luftige pandekagedej, der giver små, flotte 
             pandekager – næsten som æbleskiver – som der endnu ikke er andre 
-            steder i byen. Hos mission hos Crepe Cafe er, at opgradere den klassiske
+            steder i byen. Hos mission hos Crepe Café er, at opgradere den klassiske
             pandekage og gøre lidt mere ud af det. Derfor laver vi dem også fra 
             bunden.  Vi har åbent alle ugens dage fra kl. 15, så rammer sukkertrangen 
             kort efter frokost eller skal du have lidt lækkert til eftermiddagskaffen kan
-            du bestemt  få stillet dine cravings hos Crepe Cafe Desserthus. 
+            du bestemt  få stillet dine cravings hos Crepe Café Desserthus. 
             </p>
 
             
