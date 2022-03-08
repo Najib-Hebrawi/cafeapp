63236d4469d9f232d8b938c3477bb8f6f438236f
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-08 08:02:19 +0100

    change from function to const in kontaktos page.

--- src/component/KontaktOsPage.js
+++ src/component/KontaktOsPage.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+
 import image from './images_KontaktOs/KontaktOsImage.jpg';
 import '../component/KontaktOsStyle.css';
 
@@ -7,39 +7,35 @@ import '../component/KontaktOsStyle.css';
 
 
 
-function KontaktOsPage () {
-    
-  
-
-
+const KontaktOsPage = ()=> {
     return(
 
         <div >
             <img
-             src={image}
-              alt="a image of a frugt"
-              />
-
+            src={image}
+            alt="a image of a frugt"
+            />
 
             <div class="grid-container">
             <button
-             id="b1" 
-             type="button" 
-             class="grid-item"
-             onClick={() => window.location = 'tel:+451234567'}
-             >Kontakt os  +4512345678</button>
+            id="b1" 
+            type="button" 
+            class="grid-item"
+            onClick={() => window.location = 'tel:+451234567'}
+            >Kontakt os  +4512345678</button>
 
             <button
-             id="b2" 
-             type="button"
-              class="grid-item"
-              onClick={() => window.location = 'mailto:info@crepecafe'}
-              >Skriv til os info@crepecafe.dk</button>
+            id="b2" 
+            type="button"
+            class="grid-item"
+            onClick={() => window.location = 'mailto:info@crepecafe'}
+            >Skriv til os info@crepecafe.dk</button>
 
             <button 
             id="b3" 
             type="button"
-             class="grid-item">Find os København</button>
+            class="grid-item">
+            Find os København</button>
 
             </div>
             
