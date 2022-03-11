a6b1f9619dfe770df15b35ea408ea82c25a6e19b
DragonHeart <s195462@student.dtu.dk>
2022-03-11 09:18:23 +0100

    added ContactUsPage

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+//import image from './images_KontaktOs/KontaktOsImage.jpg';
 import '../App.css';
 
 function ContactUsPage(){
 
     return(
-        <h1>Hello From Contact us</h1>
+        <div >
+            <img
+                src={"image"}
+                alt="a image of a frugt"
+            />
+
+            <div class="grid-container">
+                <button
+                    id="b1"
+                    type="button"
+                    class="grid-item"
+                    onClick={() => window.location = 'tel:+451234567'}
+                >Kontakt os  +4512345678</button>
+
+                <button
+                    id="b2"
+                    type="button"
+                    class="grid-item"
+                    onClick={() => window.location = 'mailto:info@crepecafe'}
+                >Skriv til os info@crepecafe.dk</button>
+
+                <button
+                    id="b3"
+                    type="button"
+                    class="grid-item">
+                    Find os København</button>
+
+            </div>
+
+
+        </div>
     )
 }
 
