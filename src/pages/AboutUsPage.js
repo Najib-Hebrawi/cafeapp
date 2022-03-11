b3c723b05fd44a95a95ebb3900dd57294244494f
DragonHeart <s195462@student.dtu.dk>
2022-03-11 09:57:34 +0100

    added about us

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,16 +1,44 @@
 import React from "react";
 import '../App.css';
+import './AboutUsPage.css'
 
 function AboutUsPage(){
     return(
-        <>
-            <h1>Hello From About us</h1>
+        <div>
+
+            <img className="img_view"
+                src={"images_om_os/Om_Os_Picture.jpg"}
+                alt="A image of a fruit"
+            />
+
+            <div className="containers">
+
+                <h1 className="head">
+                    DESSERTHUS I HJERTET AF NØRREBRO!
+                </h1>
+                <p className="text">
+                    Crepe Café laver selv den luftige pandekagedej, der giver små, flotte
+                    pandekager – næsten som æbleskiver – som der endnu ikke er andre
+                    steder i byen. Hos mission hos Crepe Café er, at opgradere den klassiske
+                    pandekage og gøre lidt mere ud af det. Derfor laver vi dem også fra
+                    bunden.  Vi har åbent alle ugens dage fra kl. 15, så rammer sukkertrangen
+                    kort efter frokost eller skal du have lidt lækkert til eftermiddagskaffen kan
+                    du bestemt  få stillet dine cravings hos Crepe Café Desserthus.
+                </p>
+
+            </div>
 
 
-        </>
+
+        </div>
+
 
     )
 }
 
 
 export default AboutUsPage
+
+
+
+
