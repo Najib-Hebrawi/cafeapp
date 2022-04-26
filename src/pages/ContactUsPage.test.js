14832794676b7743dbddd5b24d4c3ed238da847e
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-26 15:21:46 +0200

    test with method toHaveTextContent

---
+++ src/pages/ContactUsPage.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import ContactUsPage from './ContactUsPage.js';
+
+
+// test the link that refer to learn react
+test('renders learn react link', () => {
+  render(<ContactUsPage />);
+  const linkElement = screen.getByText(/Find os København/i); // /Find os København/i : this is because it is regular expression that makes it case insensitive.
+  expect(linkElement).toHaveTextContent;
+});
