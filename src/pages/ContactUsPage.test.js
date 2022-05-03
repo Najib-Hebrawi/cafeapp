73f29b74ff9dd877c6e61c871d50505734e0fd8f
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-05-03 21:45:04 +0200

    .

--- src/pages/ContactUsPage.test.js
+++ src/pages/ContactUsPage.test.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import ContactUsPage from './ContactUsPage.js';
 
 
-// test the link that refer to learn react
-test('renders learn react link', () => {
+// test the link that refer to Find os København
+test('renders Find os København text', () => {
   render(<ContactUsPage />);
   const linkElement = screen.getByText(/Find os København/i); // /Find os København/i : this is because it is regular expression that makes it case insensitive.
   expect(linkElement).toHaveTextContent;
