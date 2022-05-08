c2df630b85b1976a7fb580bb6d332b9761df3fee
Dragon <s195462@student.dtu.dk>
2022-05-08 12:19:58 +0200

    added comments to Slider.js

--- src/client/component/home/slider/Slider.js
+++ src/client/component/home/slider/Slider.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import { sliderItems } from "./data";
-
+//full screen slider
 const Container = styled.div`
   width: 100%;
   height: 70vh;
@@ -10,7 +10,7 @@ const Container = styled.div`
   position: relative;
   overflow: hidden;
 `;
-
+//we have created two arrow
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
@@ -29,11 +29,12 @@ const Arrow = styled.div`
   opacity: 0.5;
   z-index: 2;
 `;
-
+//move wrapper left and right
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
   transition: all 1.5s ease;
+  //first slide will be 0 * -100 => 0 , second will be 1 * -100 => -100  and so on 
   transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
@@ -74,17 +75,19 @@ const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
   const handleClick = (direction) => {
     if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2); //if first slide go to the last item, first slide "-1" , last slide "2"
     } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0); // if > then 2 we can go the right silde
     }
   };
 
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
+        {/*  from material ui*/}
         <ArrowLeftOutlined />
       </Arrow>
+
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
           <Slide bg={item.bg} key={item.id}>
@@ -98,7 +101,9 @@ const Slider = () => {
           </Slide>
         ))}
       </Wrapper>
+
       <Arrow direction="right" onClick={() => handleClick("right")}>
+          {/*  from material ui*/}
         <ArrowRightOutlined />
       </Arrow>
     </Container>
